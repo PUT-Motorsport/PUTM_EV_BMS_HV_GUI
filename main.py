c309e926613084d75736f4cd86b0c9eff833e33d
KozAAAAA <mateusz-koza@outlook.com>
2023-08-21 14:31:02 +0200

    Send to and receive safely from serial, Change layout, Log ok, warning and error messages in different colors

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import threading
 import sys
 import PySimpleGUI as sg
 import serial
+from colorama import Fore, Style
 
 sg.theme("Dark2")
 
@@ -110,55 +111,99 @@ temperature = [
 ]
 
 charge_control = [[sg.Button("Start Charging")], [sg.Button("Stop Charging")]]
+exit_button = [[sg.Button("Exit")]]
 
 frame_basic_info = [sg.Frame("Basic Info", basic_info)]
 frame_charge_control = [sg.Frame("Charge control", charge_control)]
 
+frame_exit_button = [sg.Frame("Exit", exit_button)]
 frame_cell_voltage = [sg.Frame("Cell Voltages", cell_voltage)]
 frame_temperature = [sg.Frame("Temperatures", temperature)]
 
-column_basic_info_charge_control = sg.Column([frame_basic_info, frame_charge_control], element_justification="c")
-column_tables = sg.Column([frame_cell_voltage, frame_temperature], element_justification="c")
+column_left = sg.Column([frame_basic_info, frame_charge_control], element_justification="l", vertical_alignment="top")
+column_right = sg.Column([frame_cell_voltage, frame_temperature, frame_exit_button], element_justification="r", vertical_alignment="top")
 
 layout = [
-    [column_basic_info_charge_control, column_tables],
+    [column_left, column_right],
 ]
-window = sg.Window("BMS HV Utility", layout, element_justification="c")
+window = sg.Window("BMS HV Utility", layout, element_justification="l")
 
+def print_ok(msg):
+    """Prints an ok message"""
+    print(f"{Fore.GREEN}{msg}{Style.RESET_ALL}")
+
+def print_error(msg):
+    """Prints an error message"""
+    print(f"{Fore.RED}{msg}{Style.RESET_ALL}")
+
+def print_warning(msg):
+    """Prints a warning message"""
+    print(f"{Fore.YELLOW}{msg}{Style.RESET_ALL}")
 
 def to_matrix(l, columns):
     """Converts a list to a matrix with the specified number of columns"""
     return [l[i : i + columns] for i in range(0, len(l), columns)]
 
-
-def serial_task(ser, q):
-    """Reads serial data from the specified serial port and puts it into the specified queue"""
+def serial_task(ser, read_queue, write_queue):
+    ''' This function is used to read data from the serial port and to write data to the serial port '''
+    WRITE_PREFIX = "WRITE: "
+    READ_PREFIX = "READ: "
     while True:
-        ser.reset_input_buffer()
-        ser.readline()
-        line = ser.readline().decode("utf-8")
-        q.put(line)
+        # Write data
+        try:
+            data = write_queue.get_nowait()
+            ser.write(data.encode("utf-8"))
+            print_ok(WRITE_PREFIX + "New data sent to the serial port")
+        except queue.Empty:
+            print_warning(WRITE_PREFIX + "Nothing to send to the serial port")
 
-
-if __name__ == "__main__":
+        # Read data
+        try:
+            ser.reset_input_buffer()
+            ser.readline()
+            line = ser.readline().decode("utf-8")
+            if(line == ""):
+                print_warning(READ_PREFIX + "Nothing received from the serial port")
+                continue
+            read_queue.put_nowait(line)
+            print_ok(READ_PREFIX + "New data received from the serial port")
+        except queue.Full:
+            print_warning("Read queue is full")
+            
+def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 pyserial_tests.py <serial_port>")
+        print_error("Usage: python3 pyserial_tests.py <serial_port>")
         sys.exit(1)
 
     ser = serial.Serial()
     ser.port = sys.argv[1]
+    ser.timeout = 0.1
     ser.open()
+    
     bms_hv_data_queue = queue.Queue(maxsize=1)
+    bms_hv_settings_queue = queue.Queue(maxsize=1)
+
     threading.Thread(
-        target=serial_task, args=(ser, bms_hv_data_queue), daemon=True
+        target=serial_task, args=(ser, bms_hv_data_queue, bms_hv_settings_queue), daemon=True
     ).start()
 
     while True:
-        event, values = window.read(timeout=300)
+        event, values = window.read(timeout=1000)
 
-        if event == sg.WINDOW_CLOSED:
-            ser.close()
+        if event == sg.WINDOW_CLOSED or event == "Exit":
             break;
+
+        elif event == "Start Charging":
+            try:
+                bms_hv_settings_queue.put_nowait("a")
+            except queue.Full:
+                print_error("Previous command still in the queue")
+        
+        elif event == "Stop Charging":
+            try:
+                bms_hv_settings_queue.put_nowait("b")
+            except queue.Full:
+                print_error("Previous command still in the queue")
 
         elif not bms_hv_data_queue.empty():
             bms_hv_data_json = bms_hv_data_queue.get()
@@ -182,10 +227,14 @@ if __name__ == "__main__":
                 )
 
             except json.decoder.JSONDecodeError:
-                print("Invalid JSON: " + bms_hv_data_json)
-                continue
-            except TypeError:
-                print("Received JSON is not of type BmsHvData: " + bms_hv_data_json)
-                continue
+                print_error("Invalid JSON: " + bms_hv_data_json)
 
+            except TypeError:
+                print_error("Received JSON is not of type BmsHvData: " + bms_hv_data_json)
+
+    ser.close()
     window.close()
+            
+
+if __name__ == "__main__":
+    main()
